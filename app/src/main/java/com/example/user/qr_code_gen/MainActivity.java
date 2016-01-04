d3823d080ddc99f0ea773d24eee0a003cf6aa077
thomasm0077 <thomasm0077@gmail.com>
2016-01-04 14:41:52 +0530

    Commit1

--- app/src/main/java/com/example/user/qr_code_gen/MainActivity.java
+++ app/src/main/java/com/example/user/qr_code_gen/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.user.qr_code_gen;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    String str="www.google.com";
+    int WIDTH=512;
+    String BLACK="#ffff0000";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1= (Button) findViewById(R.id.b1);
+        final ImageView v1= (ImageView) findViewById(R.id.imageView1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Bitmap bitmap = encodeAsBitmap(str);
+                    v1.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? Color.BLUE : Color.WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 512, 0, 0, w, h);
+        return bitmap;
     }
 }
